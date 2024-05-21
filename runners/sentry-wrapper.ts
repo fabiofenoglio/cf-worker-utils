f9b64bb79e5ae40b5f3c5a4e09d7748f4f74a38e
Fabio FENOGLIO <fabio@numeral.io>
2024-05-21 15:49:04 +0200

    utils update

---
+++ runners/sentry-wrapper.ts
@@ -0,0 +1,45 @@
+import { Toucan } from 'toucan-js';
+
+export async function handleFetchWithSentry<Env = unknown, CfHostMetadata = unknown>(
+    input: {
+        sentryDSN: string;
+        workerName: string;
+    }, 
+    handler: ExportedHandlerFetchHandler<Env, CfHostMetadata>,
+    args: {
+        req: Request<CfHostMetadata, IncomingRequestCfProperties<CfHostMetadata>>;
+        env: Env;
+        ctx: ExecutionContext;
+    },
+) {
+    const sentry = new Toucan({
+        dsn: input.sentryDSN,
+        context: args.ctx,
+        request: args.req,
+        beforeSend: (event) => {
+            event.tags = {
+                ...(event.tags || {}),
+                'worker': input.workerName,
+            };
+            event.contexts = {
+                ...(event.contexts || {}),
+                'worker': {
+                    'name': input.workerName,
+                }
+            };
+            event.fingerprint = [
+                'worker:' + input.workerName,
+                ...(event.fingerprint || ['{{ default }}']),
+            ];
+            return event;
+        },
+    });
+
+    try {
+        return await handler(args.req, args.env, args.ctx);
+    } catch (err) {
+        sentry.captureException(err);
+
+        throw err;
+    }
+}
